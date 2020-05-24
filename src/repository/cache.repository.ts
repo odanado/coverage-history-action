90189791fd9ab5e52b750b9ef5ebe8bb0b11575a
odan <odan3240@gmail.com>
2020-05-25 00:17:16 +0900

    feat(cache.repository): match prefix key

--- src/repository/cache.repository.ts
+++ src/repository/cache.repository.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { saveCache, restoreCache } from "@actions/cache";
+import { context } from "@actions/github";
 
 import { CoverageResult } from "../type";
 import { Repository } from "./index";
@@ -9,32 +10,41 @@ export class CacheRepository implements Repository {
   private getDirectory(): string {
     return ".coverage-history";
   }
-  private getFileNmae(branch: string): string {
+
+  private getfileName(branch: string): string {
     return path.join(this.getDirectory(), `${branch}.json`);
   }
+
+  private getKey(branch: string): string {
+    const key = ["coverage-history-action", branch, "coverage"].join(":");
+    return key;
+  }
+
   async save(branch: string, value: unknown): Promise<void> {
     const directory = this.getDirectory();
     const key = this.getKey(branch);
 
     await fs.promises.mkdir(directory, { recursive: true });
 
-    fs.promises.writeFile(this.getFileNmae(branch), JSON.stringify(value));
+    fs.promises.writeFile(this.getfileName(branch), JSON.stringify(value));
 
-    await saveCache([directory], key);
+    const runId = process.env.GITHUB_RUN_ID;
+
+    await saveCache([directory], `${key}-${runId}`);
   }
 
   async load(branch: string): Promise<unknown> {
     const key = this.getKey(branch);
     const directory = this.getDirectory();
 
-    const cacheHit = restoreCache([directory], key);
+    const cacheHit = restoreCache([directory], key, [key]);
 
     if (!cacheHit) {
       return undefined;
     }
 
-    const fileNmae = this.getFileNmae(branch);
-    const value = await fs.promises.readFile(fileNmae, { encoding: "utf8" });
+    const fileName = this.getfileName(branch);
+    const value = await fs.promises.readFile(fileName, { encoding: "utf8" });
 
     console.log("load", key, value);
     try {
@@ -42,11 +52,6 @@ export class CacheRepository implements Repository {
     } catch {
       return undefined;
     }
-  }
-
-  private getKey(branch: string): string {
-    const key = ["coverage-history-action", branch, "coverage"].join(":");
-    return key;
   }
 
   async saveCoverage(branch: string, value: CoverageResult): Promise<void> {
