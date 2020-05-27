45d11081bab75306d36c14955b2e0e6e83b6e1c3
odan <odan3240@gmail.com>
2020-05-27 21:31:07 +0900

    test(markdown.reporter): add test for MarkdownReporter

---
+++ src/reporter/markdown.reporter.spec.ts
@@ -0,0 +1,38 @@
+import { MarkdownReporter } from "./markdown.reporter";
+import { CoverageSet } from "../type";
+
+describe("MarkdownReporter", () => {
+  const target: CoverageSet = {
+    statement: {
+      total: 0.1,
+      paths: [],
+    },
+    branch: {
+      total: 0.2,
+      paths: [],
+    },
+    function: {
+      total: 0.3,
+      paths: [],
+    },
+  };
+  const current: CoverageSet = {
+    statement: {
+      total: 0.4,
+      paths: [],
+    },
+    branch: {
+      total: 0.5,
+      paths: [],
+    },
+    function: {
+      total: 0.6,
+      paths: [],
+    },
+  };
+  it("report correctly", async () => {
+    const reporter = new MarkdownReporter();
+    const report = await reporter.report(target, current);
+    expect(report).toMatchSnapshot();
+  });
+});
