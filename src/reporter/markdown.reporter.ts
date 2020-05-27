82bf7cec8b349262553c4206ceafe833b30e9d62
odan <odan3240@gmail.com>
2020-05-27 21:30:47 +0900

    refactor(markdown.reporter): use template string

--- src/reporter/markdown.reporter.ts
+++ src/reporter/markdown.reporter.ts
@@ -1,6 +1,23 @@
 import { Reporter } from "./index";
 import { CoverageSet } from "../type";
 
+export function tableMaker(
+  strings: TemplateStringsArray,
+  ...placeholders: string[]
+): (values: { [key: string]: string }) => string {
+  return (values: { [key: string]: string }): string => {
+    return (
+      strings[0] +
+      placeholders
+        .map((placeholder, i) => {
+          return `${values[placeholder]}${strings[i + 1]}`;
+        })
+        .join("")
+        .trim()
+    );
+  };
+}
+
 export class MarkdownReporter implements Reporter {
   private format(coverage: number): string {
     return (coverage * 100).toFixed(2);
@@ -9,27 +26,23 @@ export class MarkdownReporter implements Reporter {
     targetCoverage: CoverageSet,
     currentCoverage: CoverageSet
   ): Promise<string> {
-    const table = `
+    const makeTable = tableMaker`
 |  | target | current |
 | - | -: | -: |
-|  statement  | ${this.format(
-      targetCoverage.statement.total
-    )} % | ${this.format(currentCoverage.statement.total)} % |
-|  branch  | ${this.format(targetCoverage.branch.total)} % | ${this.format(
-      currentCoverage.branch.total
-    )} % |
-|  function  | ${this.format(targetCoverage.function.total)} % | ${this.format(
-      currentCoverage.function.total
-    )} % |
-    `;
+|  statement  | ${"targetStatement"} | ${"currentStatement"} |
+|  branch  | ${"targetBranch"} | ${"currentBranch"} |
+|  function  | ${"targetFunction"} | ${"currentFunction"} |
+`;
+
+    const table = makeTable({
+      targetStatement: `${this.format(targetCoverage.statement.total)} %`,
+      currentStatement: `${this.format(currentCoverage.statement.total)} %`,
+      targetBranch: `${this.format(targetCoverage.branch.total)} %`,
+      currentBranch: `${this.format(currentCoverage.branch.total)} %`,
+      targetFunction: `${this.format(targetCoverage.function.total)} %`,
+      currentFunction: `${this.format(currentCoverage.function.total)} %`,
+    });
+
     return Promise.resolve(table);
   }
 }
-
-/*
-|   |  master  | improve-istanbul-loader |
-| - | -: | -: |
-|  statement  |  TD  | 30% |
-|  branch  |  TD  | 30% |
-|  function |  TD  | 30% |
-*/
