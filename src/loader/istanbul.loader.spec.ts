6fd19b060f1c806693851bc4ac555dbd73fd11f5
odan <odan3240@gmail.com>
2020-05-30 23:09:32 +0900

    test(istanbul.loader): add small testcase

--- src/loader/istanbul.loader.spec.ts
+++ src/loader/istanbul.loader.spec.ts
@@ -10,4 +10,16 @@ describe("IstanbulLoader", () => {
     expect(coverage.branch.total).toBeCloseTo(0.0417);
     expect(coverage.function.total).toBeCloseTo(0.359);
   });
+
+  describe("small case", () => {
+    const loader = new IstanbulLoader("./fixture/small");
+    it("ok", async () => {
+      const coverage = await loader.load();
+
+      expect(coverage.statement).toBeDefined();
+      expect(coverage.statement.total).toBeCloseTo(0.7778);
+      expect(coverage.branch.total).toBeCloseTo(0);
+      expect(coverage.function.total).toBeCloseTo(0.75);
+    });
+  });
 });
